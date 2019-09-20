79c3cad0324807b79cfb1ff76aa5fe296d68fde5
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-09-20 11:17:42 -0700

    Add movie carousels

--- src/components/Carousels/MovieCarousels.js
+++ src/components/Carousels/MovieCarousels.js
@@ -1,9 +1,97 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Swiper from 'swiper';
 import { Link } from 'react-router-dom';
+import { movieGenre, TvGenre } from '../../constants/Genre';
 
-const MovieCarousels = () => {
-  return <div></div>;
+const MovieCarousels = ({ movies, type = 'movie' }) => {
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    const slider = new Swiper(swiperRef.current, {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      slidesPerGroup: 1,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      // breakpoints: {
+      //   1145: {
+      //     slidesPerView: 3
+      //   },
+      //   699: {
+      //     slidesPerView: 2
+      //   }
+      // },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    });
+  });
+
+  const helper = id => {
+    let result = '';
+    if (type === 'movie') {
+      movieGenre.genres.forEach(genre => {
+        if (genre.id === id) {
+          result = genre.name;
+        }
+      });
+    } else {
+      TvGenre.genres.forEach(genre => {
+        if (genre.id === id) {
+          result = genre.name;
+        }
+      });
+    }
+
+    return result;
+  };
+
+  return (
+    <div className="swiper-container" ref={swiperRef}>
+      <div className="swiper-wrapper">
+        {movies.map(movie => (
+          <Link
+            to={`/${type}/details/${movie.id}`}
+            key={movie.id}
+            className="swiper-slide"
+            style={{
+              backgroundImage: `url(https://image.tmdb.org/t/p/w300/${movie.poster_path})`
+            }}>
+            <div className="carousel-movie-overlay"></div>
+            <div className="carousel-movie-info">
+              <h3 className="carousel-movie-title">
+                {type === 'tv'
+                  ? movie.name
+                      .split(' ')
+                      .splice(0, 10)
+                      .join(' ') + '...'
+                  : movie.title
+                      .split(' ')
+                      .slice(0, 10)
+                      .join(' ')}
+              </h3>
+              <p className="carousel-movie-genre">
+                {movie.genre_ids.map(id => helper(id)).join(', ')}
+              </p>
+              <div className="carousel-movie-vote">
+                <p>{movie.vote_average}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+
+      <div className="swiper-pagination" />
+
+      <div className="swiper-button-next" />
+      <div className="swiper-button-prev" />
+    </div>
+  );
 };
 
 export default MovieCarousels;
--- src/components/Landing/MainContent.js
+++ src/components/Landing/MainContent.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getPopular, getTopRated, getNowPlaying } from '../../actions/movie';
+import MovieCarousels from '../Carousels/MovieCarousels';
+import { connect } from 'react-redux';
 
-const MainContent = () => {
+const MainContent = ({
+  getPopular,
+  popular,
+  nowPlaying,
+  loading,
+  getTopRated,
+  getNowPlaying,
+  topRated
+}) => {
+  useEffect(() => {
+    getPopular();
+    getNowPlaying();
+    getTopRated();
+  }, [getPopular, getTopRated, getNowPlaying]);
+
   return (
     <div className="main-content">
       <div className="container">
-        <div className="section-movie-carousel">
-          <h2 className="section-heading">Popular Movies</h2>
+        <div className="type-selector">
+          <label htmlFor="type-toggle">
+            <input type="checkbox" id="type-toggle" />
+            <span className="type-holder">
+              <span className="type-movie">Movie</span>
+              <span className="type-tv">TV shows</span>
+              <span className="slider"></span>
+            </span>
+          </label>
+        </div>
+        <div className="section-movie-list">
+          <div className="movie-carousel-container mb-2">
+            <h2 className="section-heading mb-1">Popular Movies</h2>
+            {loading.GET_POPULAR ? null : <MovieCarousels movies={popular} />}
+          </div>
+          <div className="movie-carousel-container mb-2">
+            <h2 className="section-heading mb-1">Now Playing Movies</h2>
+            {loading.GET_NOW_PLAYING ? null : (
+              <MovieCarousels movies={nowPlaying} />
+            )}
+          </div>
+          <div className="movie-carousel-container">
+            <h2 className="section-heading mb-1">Top Rated Movies</h2>
+            {loading.GET_TOP_RATED ? null : (
+              <MovieCarousels movies={topRated} />
+            )}
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default MainContent;
+const mapStateToProps = state => ({
+  popular: state.movie.movie.GET_POPULAR,
+  nowPlaying: state.movie.movie.GET_NOW_PLAYING,
+  topRated: state.movie.movie.GET_TOP_RATED,
+  loading: state.movie.loading
+});
+
+export default connect(
+  mapStateToProps,
+  { getPopular, getTopRated, getNowPlaying }
+)(MainContent);
