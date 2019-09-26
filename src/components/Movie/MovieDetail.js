c45881a7715f1a24f678d5e47cc8d1b1e99d8f8b
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-09-25 22:37:21 -0700

    Add and style header bottom info in movie details

--- src/components/Movie/MovieDetail.js
+++ src/components/Movie/MovieDetail.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { getDetail } from '../../actions/movie';
-import { connect } from 'react-redux';
-import Spinner from '../Layout/Spinner';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { getDetail } from "../../actions/movie";
+import { connect } from "react-redux";
+import Spinner from "../Layout/Spinner";
+import { Link } from "react-router-dom";
 
 const MovieDetail = ({ match, getDetail, details, loading }) => {
   useEffect(() => {
@@ -17,12 +17,14 @@ const MovieDetail = ({ match, getDetail, details, loading }) => {
           className="background-blur"
           style={{
             backgroundImage: `url(https://image.tmdb.org/t/p/w1280${details.backdrop_path}`
-          }}></div>
+          }}
+        ></div>
         <div
           className="background-main"
           style={{
             backgroundImage: `url(https://image.tmdb.org/t/p/w1280${details.backdrop_path}`
-          }}>
+          }}
+        >
           <div className="movie-poster">
             <img
               src={`https://image.tmdb.org/t/p/w1280${details.poster_path}`}
@@ -31,14 +33,14 @@ const MovieDetail = ({ match, getDetail, details, loading }) => {
 
             <div className="header-movie-detail">
               <p className="header-movie-detail-release">
-                {details.tagline || 'No tagline found'}
+                {details.tagline || "No tagline found"}
               </p>
               <h1 className="header-movie-detail-title">{details.title}</h1>
 
               <p className="header-movie-detail-genre">
                 {details.runtime +
-                  ' min | ' +
-                  details.genres.map(genre => genre.name).join(' - ')}
+                  " min | " +
+                  details.genres.map(genre => genre.name).join(" - ")}
               </p>
 
               <Link className="btn btn-primary">
@@ -49,7 +51,36 @@ const MovieDetail = ({ match, getDetail, details, loading }) => {
           </div>
         </div>
 
-        <div className="header-bottom-detail"></div>
+        <div className="header-bottom-detail">
+          <div className="header-bottom-detail-stats">
+            <div className="item">
+              <h4 className="title">
+                <span className="text-white">{details.vote_average}</span>/10
+              </h4>
+              <p className="subtext">{details.vote_count}</p>
+            </div>
+            <div className="item">
+              <h4 className="title">Popularity</h4>
+              <p className="subtext">{details.popularity}</p>
+            </div>
+            <div className="item">
+              <h4 className="title">Revenue</h4>
+              <p className="subtext">{details.revenue}</p>
+            </div>
+          </div>
+
+          <ul className="header-bottom-detail-list">
+            <li className="item">
+              <Link to="/">Full Cast and crew</Link>
+            </li>
+            <li className="item">
+              <Link to="/">Trailer</Link>
+            </li>
+            <li className="item">
+              <Link to="/">Overview</Link>
+            </li>
+          </ul>
+        </div>
       </header>
     </>
   );
